537a1f9d327cd184a91210e950bd81a0754e5a11
j30 <46060104+joaquin30@users.noreply.github.com>
2024-07-13 11:43:52 -0500

    añadido señal de moco

--- moco/moco.gd
+++ moco/moco.gd
@@ -1,8 +1,12 @@
 extends Polygon2D
 
-const MIN_VEL = 10
-const MAX_VEL = 40
-const MOCO_VEL = 150
+signal moco_inhalation(vel_change)
+
+@export var MIN_VEL: int
+@export var MAX_VEL: int
+@export var MOCO_VEL: int
+@export var MIN_VEL_CHANGE: float
+@export var MAX_VEL_CHANGE: float
 var yvel = randi_range(MIN_VEL, MAX_VEL)
 var other: String
 
@@ -15,8 +19,12 @@ func _ready():
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if !Input.is_action_pressed("limpiar_moco_"+other) and Input.is_action_pressed("limpiar_moco_"+name):
+func _process(delta: float):
+	if Input.is_action_just_pressed("limpiar_moco_"+name):	
+		moco_inhalation.emit(sign(randf() - 0.5) * randf_range(MIN_VEL_CHANGE, MAX_VEL_CHANGE))
+		
+	if !Input.is_action_pressed("limpiar_moco_"+other) and \
+			Input.is_action_pressed("limpiar_moco_"+name):
 		position.y -= MOCO_VEL * delta
 		position.y = max(position.y, 0)
 	else:
