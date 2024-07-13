248329486ebd9cf8a5af7991a04df82388570faf
KuliesJ <jorge.nunez.paucar@ucsp.edu.pe>
2024-07-13 18:37:26 -0500

    Basic menu

---
+++ scripts/menu.gd
@@ -0,0 +1,26 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var controls = $Controls
+	controls.visible = false
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _on_start_button_up():
+	get_tree().change_scene_to_file("res://moco/moco.tscn")
+
+
+func _on_controles_pressed():
+	var controls = $Controls
+	controls.visible = true
+
+
+func _on_close_button_up():
+	#Close controls menu
+	var controls = $Controls
+	controls.visible = false
