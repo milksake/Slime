9aa1c167d09be6f96d2e5be908c34b8c1dd8e97c
j30 <46060104+joaquin30@users.noreply.github.com>
2024-07-13 15:47:14 -0500

    script central de moco

--- moco/moco.gd
+++ moco/moco.gd
@@ -1,61 +1,77 @@
-extends Polygon2D
+extends Node2D
 
 signal moco_vel(vel_change)
 signal game_over
 
 const MIN_VEL: int = 20
 const MAX_VEL: int = 30
-const MOCO_VEL: int = 150
+const MOCO_VEL: float = 150
 const MIN_VEL_CHANGE: float = 0.2
 const MAX_VEL_CHANGE: float = 0.5
-var pos: Vector2
-var y_real_size: float
-var y_actual_size: float
-var yvel: int
-var other: String
+
+var actual_size_izq: float
+var pos_izq: Vector2
+var vel_izq: float
+
+var actual_size_der: float
+var pos_der: Vector2
+var vel_der: float
+
+var real_size: float
 var game: bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	position = get_window().size
-	print(position)
-	if name == "izq":
-		other = "der"
-		position.x -= 50
-	else:
-		other = "izq"
-		position.x += 50
-	
-	y_real_size = 50
-	pos = position
-	yvel = randi_range(MIN_VEL, MAX_VEL)
+	$izq.position = get_window().size
+	$izq.position.x -= 50
+	$der.position = get_window().size
+	$der.position.x += 50
+	real_size = 50
+	actual_size_izq = 50
+	actual_size_der = 50
+	pos_izq = $izq.position
+	pos_der = $der.position
+	limpiar_moco()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
-	if Input.is_action_just_pressed("limpiar_moco_"+name):
+	if Input.is_action_just_pressed("limpiar_moco_izq") or \
+			Input.is_action_just_pressed("limpiar_moco_der"):
 		var vel = sign(randf() - 0.5) * randf_range(MIN_VEL_CHANGE, MAX_VEL_CHANGE)
 		moco_vel.emit(vel)
 		print("emitted ", vel)
 		
-	if !Input.is_action_pressed("limpiar_moco_"+other) and \
-			Input.is_action_pressed("limpiar_moco_"+name):
-		y_actual_size -= MOCO_VEL * delta
-		y_actual_size = max(y_actual_size, y_real_size)
+	if Input.is_action_pressed("limpiar_moco_izq") != \
+			Input.is_action_pressed("limpiar_moco_der"):
+		if Input.is_action_pressed("limpiar_moco_izq"):
+			actual_size_izq -= MOCO_VEL * delta
+			actual_size_izq = max(actual_size_izq, real_size)
+		else:
+			actual_size_der -= MOCO_VEL * delta
+			actual_size_der = max(actual_size_der, real_size)
 	else:
-		y_actual_size += yvel * delta
+		actual_size_izq += vel_izq * delta
+		actual_size_der += vel_der * delta
 	
-	scale.y = y_actual_size / y_real_size
-	position = pos
-	if Input.is_action_just_released("limpiar_moco_"+name):
+	$izq.scale.y = actual_size_izq / real_size
+	$izq.position = pos_izq
+	
+	$der.scale.y = actual_size_der / real_size
+	$der.position = pos_der
+
+	if Input.is_action_just_released("limpiar_moco_izq") or \
+			Input.is_action_just_released("limpiar_moco_der"):
 		moco_vel.emit(0)
 		print("emitted 0")
 		limpiar_moco()
 	
-	if pos.y + y_actual_size > 1100 and game:
+	if ($izq.position.y + actual_size_izq > 1100 or \
+			$der.position.y + actual_size_der > 1100) and game:
 		game_over.emit()
 		game = false
 		print("game over")
 		
 func limpiar_moco():
-	yvel = randi_range(MIN_VEL, MAX_VEL)
-	# position.y = 0
+	vel_izq = randi_range(MIN_VEL, MAX_VEL)
+	vel_der = randi_range(MIN_VEL, MAX_VEL)
+	print(vel_izq, " ", vel_der)
